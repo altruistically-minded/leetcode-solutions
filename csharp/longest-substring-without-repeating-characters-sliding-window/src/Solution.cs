8d841ff028b330a0c0a2ee28b076cafd1a5b6002
alex <altruistically.minded@gmailc.om>
2020-05-15 13:00:44 -0500

    A.Moreno - Setting up the longest substring without repeating characters solution

---
+++ csharp/longest-substring-without-repeating-characters-sliding-window/src/Solution.cs
@@ -0,0 +1,11 @@
+namespace longest_substring_without_repeating_characters_sliding_window
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+
+            return 0;
+        }
+    }
+}
---
+++ csharp/longest-substring-without-repeating-characters-sliding-window/tests/SolutionTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace longest_substring_without_repeating_characters_sliding_window
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
